25d32e140d859b62a84896f4c2f23df6496b39d0
Vattikala Pavan Kalyan <pavanvattikala54@gmail.com>
2023-04-03 00:33:07 +0530

    newsletter-removed

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -86,33 +86,5 @@
     </div>
     <!-- testimonial_area_end -->
 
-
-    <!-- subscribe_newsletter_Start -->
-    <div class="subscribe_newsletter">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-6 col-lg-6">
-                    <div class="newsletter_text">
-                        <h3>Subscribe Newsletter</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam alias esse aliquam natus quam dolore.</p>
-                    </div>
-                </div>
-                <div class="col-xl-5 offset-xl-1 col-lg-6">
-                    <div class="newsletter_form">
-                        <h4>Lorem ipsum dolor sit amet.</h4>
-                        <form action="#" class="newsletter_form">
-                            <input type="text" placeholder="Enter your mail">
-                            <button type="submit">Sign Up</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- subscribe_newsletter_end -->
-
-
-    
-
 </body>
 @endsection
